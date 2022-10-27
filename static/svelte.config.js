6e0f83fbfb2d965e567f040171220b176d26d3fe
Nico Clack <16543008+hedgehog125@users.noreply.github.com>
2022-10-27 20:10:29 +0100

    Some slight fixes

--- static/svelte.config.js
+++ static/svelte.config.js
@@ -1,10 +1,15 @@
 import adapter from "@sveltejs/adapter-static";
 
-const URL_PREFIX = "SvelteKit-Template"; // <-- Set this to the repository name if you're hosting on github pages (unless it's your homepage site), as all the URLs will need to be prefixed with it
+const URL_PREFIX = "SvelteKit-Template"; // <-- Set this to the repository name if you're hosting on GitHub Pages (unless it's your homepage site), as all the URLs will need to be prefixed with it
+const IS_TEST_BUILD = false; // If you want to do a test build, set this to true so the prefix doesn't get added
 
 
 const dev = process.env.NODE_ENV != "production";
-const baseURL = dev || URL_PREFIX == ""? "" : `/${URL_PREFIX}`;
+const baseURL = (
+	dev
+	|| IS_TEST_BUILD
+	|| URL_PREFIX == ""
+)? "" : `/${URL_PREFIX}`;
 const buildDir = "build";
 
 /** @type {import("@sveltejs/kit").Config} */
@@ -18,8 +23,10 @@ const config = {
 		},
 		alias: {
 			$util: "src/lib/util",
+			$set: "src/lib/settings",
+			$sub: "src/lib/util/subcomponents",
 			$img: "src/lib/imgs",
-			$sng: "src/lib/snds",
+			$snd: "src/lib/snds",
 			$vid: "src/lib/vids"
 		},
 
