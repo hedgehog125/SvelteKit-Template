fc3312b304c533c494f0a4465c6da2bb35773dd0
Nico Clack <16543008+hedgehog125@users.noreply.github.com>
2022-11-05 22:39:17 +0000

    Tweaked a couple of things, I think

---
+++ app/immutable/chunks/3-c6c1fdcd.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/linkDemo/_page.svelte-ec8d2453.js";export{t as component};
---
+++ app/immutable/chunks/LinkPage-c78fc264.js
@@ -0,0 +1,1 @@
+import{b as l}from"./paths-6ff7d232.js";const s=t=>(t[t.length-1]=="/"&&(t=t.slice(0,t.length-1)),l+"/"+t);export{s as l};
